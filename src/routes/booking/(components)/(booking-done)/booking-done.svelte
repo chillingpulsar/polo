75c954b8132939f7299bbcf6c842b3d71edeccf3
Mike John Eviota <mikejohneviota@Mikes-Mac-mini.local>
2025-09-13 05:33:50 +0800

    update

--- src/routes/booking/(components)/(booking-done)/booking-done.svelte
+++ src/routes/booking/(components)/(booking-done)/booking-done.svelte
@@ -13,8 +13,9 @@
 	<div class="flex max-w-lg flex-col gap-4">
 		<h1 class="text-2xl font-medium text-green-500">Booking Done 🎉</h1>
 		<p>
-			Please proceed to the site and provide the <b class="underline">transaction id</b> you may view
-			it in your email. Thank you for choosing us!
+			Please proceed to the site on your booked date and provide the <b class="underline"
+				>transaction ID</b
+			> that was sent to your email. Thank you for choosing us!
 		</p>
 		<Button onclick={() => (bookingDone = false)} class="ml-auto">
 			<IconArrowLeft class="size-4" />
